9a03d94d0115c69e956998652fa59ee6ecd8679a
Olek-Wieczorek <aleksander.wieczorek@student.uj.edu.pl>
2025-08-28 01:14:58 +0200

    update simplified
    update form now doesn't work, cuz it needs id
    status is in old (bad) version. IDK how to change it

--- app/static/js/fileUpdate.js
+++ app/static/js/fileUpdate.js
@@ -14,20 +14,6 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 })
 
-//submit button function
-document.addEventListener("DOMContentLoaded", function (){
-    const submitButton=document.getElementById('submit')
-    submitButton.addEventListener('click',function (){
-        const name=document.getElementById('fileName').value
-        const tags=getSelectedTagsIds()
-        if(!name){
-            alert("Filename field is empty")
-            return
-        }
-        updateFile(tags,name)
-    })
-})
-
 //change status button (for admin)
 document.addEventListener("DOMContentLoaded", function (){
     const statusButton=document.getElementById("changeStatus")
@@ -36,39 +22,6 @@ document.addEventListener("DOMContentLoaded", function (){
         changeStatus(newStatus)
     })
 })
-
-function getSelectedTagsIds(){
-    const selectedTags = document.querySelectorAll('#selectedTags li[data-tag-id]')
-    const tagIds=[]
-    selectedTags.forEach(tag => {
-        tagIds.push(tag.getAttribute('data-tag-id'))
-    })
-    return tagIds
-}
-
-//func sending update form
-async function updateFile(tags,name){
-    const formData=new FormData()
-    formData.append("file_id",fileId)
-    formData.append("tags",JSON.stringify(tags));
-    formData.append("name",name)
-
-    //TODO: Include currently logged-in user role
-    await fetch(`/test/auth/set-role/${role}`,{
-        method: "GET"
-    })
-
-    const res=await fetch(`/${role}/update_file`,{
-        method: "POST",
-        body: formData
-    })
-
-    if(!res.ok){
-        throw new Error("Update failed")
-    }
-
-    window.location.reload();
-}
 
 //status changer
 async function changeStatus(status) {
